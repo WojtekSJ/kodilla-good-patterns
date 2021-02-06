bb47ff2c5b67c1cfa0fb98a29a58e1e7d5c5bbb1
WojtekSJ <wojnad@o2.pl>
2021-02-06 18:47:19 +0100

    13.1 Stream po filmach

---
+++ src/main/java/com/kodilla/good/patterns/challenges/MainClass.java
@@ -0,0 +1,12 @@
+package com.kodilla.good.patterns.challenges;
+
+public class MainClass {
+    public static void main(String[] args) {
+       MovieStore mov1 = new MovieStore();
+
+        mov1.getMovies().entrySet().stream()
+                .flatMap(e -> e.getValue().stream())
+                .map(e -> e + "! ")
+                .forEach(System.out::print);
+    }
+}
